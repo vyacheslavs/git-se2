c176793537ff9e4df656d43a6537986b543659da
Viacheslav Shegai <viacheslav.shegai@ienso.com>
2024-06-16 17:52:58 -0400

    Introduce Repository class
    This changeset introduces a new `Repository` class to the
    project.   In the `CMakeLists.txt` file, the `repository.h`
    and `repository.cpp` files are added to the list of sources
    for the QML module.  In the `repository.cpp` file, the
    `Repository` class is defined with a static method `open()`
    that initializes the LibGit2 library, opens a repository at
    a specified path, and handles errors related to repository
    opening. The class also defines a destructor that frees the
    repository when the object is destroyed.  In the
    `repository.h` file, the `Repository` class is declared
    within the `gitse2` namespace. It includes a shared pointer
    type `RepositoryRef` for managing instances of the
    `Repository` class. The class contains a static method
    `open()` for opening a repository and a private constructor,
    destructor, and member variables for managing the Git
    repository.

--- repository.cpp
+++ repository.cpp
@@ -25,3 +25,34 @@ private:
 };
 
 Q_GLOBAL_STATIC(LibGit2Initializer, _initializer)
+
+static std::string explain_repository_open_fail(const Error& e) {
+    const git_error *last_error = giterr_last();
+
+    std::stringstream ss;
+
+    ss << explain_generic(e);
+    ss << "opening repository failed, error = " << std::any_cast<int>(e.aux);
+    if (last_error) {
+        ss << ", last_error: "<<last_error->message<<"\n";
+    }
+
+    return ss.str();
+}
+
+Result<RepositoryRef> Repository::open()
+{
+    _initializer->init();
+    RepositoryRef out_ref(new Repository());
+
+    int error = git_repository_open_ext(&out_ref->m_repo, out_ref->m_repo_path.c_str(), 0, NULL);
+    if (error != 0 || !out_ref->m_repo)
+        return unexpected_explained(ErrorCode::GitRepositoryOpenError, explain_repository_open_fail, error);
+
+    return {};
+}
+
+Repository::~Repository()
+{
+    git_repository_free(m_repo);
+}
--- repository.h
+++ repository.h
@@ -6,4 +6,23 @@
 #include <git2.h>
 #include <QtCore/qglobal.h>
 
+namespace gitse2 {
+
+    using RepositoryRef = std::shared_ptr<class Repository>;
+
+    class Repository
+    {
+        public:
+            Q_DISABLE_COPY_MOVE(Repository)
+
+            static Result<RepositoryRef> open();
+            ~Repository();
+        private:
+            Repository() = default;
+
+            std::string m_repo_path {"."};
+            git_repository* m_repo;
+    };
+}
+
 #endif // REPOSITORY_H
